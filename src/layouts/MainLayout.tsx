6324cb12880cffa55f56c50a2af1c26bec057cbd
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-04-14 02:48:45 +0900

    feat: 메인 레이아웃 헤더 추가

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,28 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { FaBars, FaBell, FaUser } from "react-icons/fa";
+import PixabayLogo from "@/components/PixabayLogo";
 
 export default function MainLayout() {
   return (
-    <div className="bg-red-300 w-screen">
-      <Outlet />
+    <div className="w-screen p-4">
+      <header className="w-full flex justify-between gap-3 items-center h-10 bg-green-300 mb-7">
+        <PixabayLogo className="aspect-square h-full" />
+        <section className="flex gap-1">
+          <Button variant="ghost" className="rounded-full w-10 h-10">
+            <FaBell />
+          </Button>
+          <Button variant="ghost" className="rounded-full w-10 h-10">
+            <FaUser />
+          </Button>
+          <Button variant="ghost" className="rounded-full w-10 h-10">
+            <FaBars className="aspect-square h-full" />
+          </Button>
+        </section>
+      </header>
+      <main className="w-full">
+        <Outlet />
+      </main>
     </div>
   );
 }
