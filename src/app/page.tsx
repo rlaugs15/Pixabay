95fbe93523ea4102565b542e4b2b4470b3b04574
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-05-24 16:52:23 +0900

    feat: fillterTab 섹션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FillterTabSection from "@/components/features/home/FillterTabSection/FillterTabSection";
 import HomeHero from "@/components/features/home/HomeHero/HomeHero";
 
 export default async function Home() {
   return (
     <main className="w-full">
       <HomeHero />
+      <FillterTabSection />
     </main>
   );
 }
