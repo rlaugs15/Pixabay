ec8554db84b65296fcdd211cab7c02f37f3b2f3c
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2025-06-25 18:40:48 +0900

    refactor: 검색 폼 로직 수정

--- src/components/common/form/SearchForm.tsx
+++ src/components/common/form/SearchForm.tsx
@@ -1,24 +1,30 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { searchSchema } from "@/lib/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import SearchSubmitClient from "./SearchSubmitClient";
 
 export default function SearchForm() {
+  const router = useRouter();
   const form = useForm<z.infer<typeof searchSchema>>({
     resolver: zodResolver(searchSchema),
     defaultValues: {
       query: "",
     },
   });
+  const onSearchSubmit = ({ query }: z.infer<typeof searchSchema>) => {
+    router.push(`/search/${query}`);
+  };
   return (
     <section className="w-full max-w-205">
       <Form {...form}>
         <form
+          onSubmit={form.handleSubmit(onSearchSubmit)}
           className="bg-white backdrop-blur-md rounded-3xl overflow-hidden flex items-center
     ring-1 ring-slate-400 focus-within:ring-2 focus-within:ring-slate-500 focus-within:ring-offset-2 transition"
         >
@@ -33,7 +39,7 @@ export default function SearchForm() {
               </FormItem>
             )}
           />
-          <SearchSubmitClient getValue={() => form.getValues("query")} />
+          <Button className="rounded-3xl m-0.5">검색</Button>
         </form>
       </Form>
     </section>
